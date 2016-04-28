c7732aa1402881297dcaa1928de08dc8c4b9fb25
Theodor Dimov <theodor.dimov@gmail.com>
2016-04-28 15:48:00 +0200

    rajout du fichier de constantes

---
+++ config.h
@@ -0,0 +1,12 @@
+#define CONNECT		0
+#define WAIT		1
+#define DISCONNECT	2
+#define DEAL		3
+#define ASK		4
+#define PLAY		5
+#define GIVE		6
+#define EMPTY		7
+#define ROUND		8
+#define SCORE		9
+#define SCORES		10
+#define WINNER		11 
